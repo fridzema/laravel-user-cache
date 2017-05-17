7b7e980dc16a4885c8a1e368877c86b80377135f
Robert Fridzema <robertfridzema@Roberts-MacBook-Pro.local>
2017-05-17 20:20:52 +0200

    .

---
+++ src/Auth/CacheableAuthUserServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+namespace Fridzema\Auth;
+
+use Illuminate\Support\ServiceProvider;
+
+class CacheableAuthUserServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app['auth']->provider('cacheableEloquent',
+            function ($app, $config) {
+                $config['model']::updated(function ($model) {
+                    CacheableEloquentUserProvider::clearCache($model);
+                });
+                return new CacheableEloquentUserProvider($app['hash'], $config['model']);
+            }
+        );
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+}
